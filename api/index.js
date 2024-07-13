1b7afb67c0e61a866132de5639094a54ac0fbe7e
shaolongli <shaolongli26@gmail.com>
2024-07-13 12:37:11 +0800

    connect to mongo

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
