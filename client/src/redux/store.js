0842a0709a52999ba09246df2914a76e56886f04
shaolongli <shaolongli26@gmail.com>
2024-07-19 14:05:24 +0800

    change color mode

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import themeReducer from "./theme/themeSlice";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer
 });
 
 const persistConfig = {
