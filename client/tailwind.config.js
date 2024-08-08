c793afc079bbd7a2f7c8688adced21550b938dc6
shaolongli <shaolongli26@gmail.com>
2024-08-08 16:02:22 +0800

    show user post

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 import flowbite from "flowbite-react/tailwind";
+import tailwindScrollbar from "tailwind-scrollbar";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin()],
+  plugins: [flowbite, tailwindScrollbar],
 };
