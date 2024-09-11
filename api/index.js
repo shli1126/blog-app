24816745007b73d1942076daae3c0ef97b1aaf16
Shaolong Li <shaolongli26@gmail.com>
2024-09-11 11:33:51 -0700

    render

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -22,12 +23,22 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get(
+  ("*",
+  (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  })
+);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
